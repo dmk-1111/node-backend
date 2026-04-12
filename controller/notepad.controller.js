a44e8d2d43212cecc9db35fc893982ac7631beb7
mr-dark <motedee02@gmail.com>
2026-04-12 09:34:58 +0700

    Updated

--- controller/notepad.controller.js
+++ controller/notepad.controller.js
@@ -15,10 +15,56 @@ const createNote = (req,res) => {
             res.status(201).json({ message: 'Note created successfully' });
         }
     });
-
 }
+
+const updateNote = (req,res) => {
+    const sql = `UPDATE notepad SET title = $1 WHERE id = $2`;
+    const values = [req.body.title, req.params.id];
+    connection.query(sql, values, (err, result) => {
+        if (err) {
+            console.error('Error updating note:', err);
+            res.status(500).json({ error: 'Failed to update note' });
+        } else {
+            res.status(200).json({ message: 'Note updated successfully' });
+        }
+    });
+}
+
+const deleteNote = (req,res) => {
+    const sql = `DELETE FROM notepad WHERE id = $1`;
+    const values = [req.params.id];
+    connection.query(sql, values, (err, result) => {
+        if (err) {
+            console.error('Error deleting note:', err);
+            res.status(500).json({ error: 'Failed to delete note' });
+        } else {
+            res.status(200).json({ message: 'Note deleted successfully' });
+        }
+    });
+}
+
+const search = (req,res) => {
+    const sql = `SELECT * FROM notepad WHERE id = $1`;
+    const values = [req.query.id];
+    connection.query(sql, values, (err, result) => {
+        if (err) {
+            console.error('Error searching notes:', err);
+            res.status(500).json({ error: 'Failed to search notes' });
+        }else if (result.rows.length === 0) {
+            res.status(404).json({ message: 'No notes found matching the search query' });
+        } 
+        else {
+            res.status(200).json({ notes: result.rows });
+        }
+    });
+}
+
+
 
 module.exports = {
     home,
-    createNote
+    createNote,
+    updateNote,
+    deleteNote,
+    search
 }
--- routes/api.route.js
+++ routes/api.route.js
@@ -1,8 +1,11 @@
 const Router = require('router')
 const router = Router();
-const { home,createNote } = require('../controller/notepad.controller');
+const { home,createNote,updateNote,deleteNote,search } = require('../controller/notepad.controller');
 
 router.get('/', (req,res) => home(req,res));
 router.post('/notes', (req,res) => createNote(req,res));
+router.put('/notes/:id', (req,res) => updateNote(req,res));
+router.delete('/notes/:id', (req,res) => deleteNote(req,res));
+router.get('/search', (req,res) => search(req,res));
 
 module.exports = router;
