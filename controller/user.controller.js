e830b979fd8253d14a08cc161fc58248e8acb449
mr-dark <motedee02@gmail.com>
2026-04-14 16:42:34 +0700

    Prisma crud

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -16,3 +16,44 @@ export const createUser = async (req, res) => {
         res.status(500).json({ error: 'Failed to create user' + error });
     }
 }
+
+export const updateUser = async (req, res) => {
+    const { name, email } = req.body;
+    try {
+        const user = await prisma.$queryRaw`
+            UPDATE "User"
+            SET "email" = ${email}, "name" = ${name}
+            WHERE "id" = ${req.params.id}
+            RETURNING *;
+        `;
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update user' + error });
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    try {
+        const user = await prisma.$queryRaw`
+            DELETE FROM "User"
+            WHERE "id" = ${req.params.id}
+            RETURNING *;
+        `;
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to delete user' + error });
+    }
+}
+  
+export const searchUsers = async (req, res) => {
+    const search = req.query.keyword;
+    try {
+        const user = await prisma.$queryRaw`
+            SELECT * FROM "User"
+            WHERE "name" ILIKE ${`%${search}%`} OR "email" ILIKE ${`%${search}%`};
+        `;
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to search users' + error });
+    }
+}
--- routes/api.route.js
+++ routes/api.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createUser,welcome } from '../controller/user.controller.js';
+import { createUser,welcome,updateUser,deleteUser,searchUsers } from '../controller/user.controller.js';
 
 const router = express.Router();
 
 router.get("/", (req, res) => welcome(req,res));
 router.post("/users", (req,res) => createUser(req,res));
-
+router.put("/users/:id", (req,res) => updateUser(req,res));
+router.delete("/users/:id", (req,res) => deleteUser(req,res));
+router.get("/users", (req,res) => searchUsers(req,res));
 export default router;
